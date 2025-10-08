0ba2a4eed8d3c6e8b3cd36a4f2f6104e43aa37d7
Hugooortizzz <hugo.ortiz-duran@iesruizgijon.com>
2025-10-08 22:41:58 +0200

    8/10_2

--- PROG/Tema 5/Ejercicio20.java
+++ PROG/Tema 5/Ejercicio20.java
@@ -1,27 +1,3 @@
-/*
- * Ejercicio20.java
- * 
- * Copyright 2025 hugom <hugom@HUGO_PORTATIL>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * Igual que el ejercicio anterior pero esta vez se debe pintar una pirámide hueca.
- */
-
-
 import java.util.Scanner;
 
 public class Ejercicio20 {
@@ -38,24 +14,34 @@ public class Ejercicio20 {
 		System.out.print("Introduzca el carácter del que se va a formar la pirámide: ");
 		caracter = s.next();
 		
-		
-		for (int l = 0; l <= altura * 2; l++){
+		//Espacios de la primera línea
+		for (int l = 1; l <= altura * 2; l++){
 			System.out.print(" ");
 		}
-		System.out.print(caracter + "\n");
+		System.out.print(caracter + "\n"); //Caracter de la primera línea
 
 
-		for (int i = 1; i < altura; i++){
-			for (int k = i; k <= altura * 2; k++){
+		//Cuantas líneas va a tener
+		for (int i = 2; i < altura; i++){
+			for (int k = i; k < altura * 2; k++){ //Los espacios a la izquierda
 					System.out.print(" ");
 			}
-			System.out.print(caracter);
-			for (int j = 1; j <= i*2; j++){
+			System.out.print(caracter); 
+			for (int j = 1; j < i*2; j++){ //Los espacios entre el primer y el segundo carácter
 				System.out.print(" ");
 			}
+			System.out.print(caracter + "\n");//El último caracter
+		}
+		
+		for (int p = 0; p < altura; p++){ //Espacios a la izquierda de la última línea
+			System.out.print(" ");
+		}
+
+		for (int n = 0; n <= altura * 2; n++){//Caracteres de la última línea
 			System.out.print(caracter);
-			System.out.print("\n");
-		}	
+		}
 	}
 }
+
+
 
