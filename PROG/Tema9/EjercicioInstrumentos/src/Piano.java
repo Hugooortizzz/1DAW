ca2fcd8895dfc22cf881686721cd31cb7b705a1c
Hugooortizzz <hugo.ortiz-duran@iesruizgijon.com>
2026-03-22 19:29:12 +0100

    22/03/26_2_2

--- PROG/Tema9/EjercicioInstrumentos/src/Piano.java
+++ PROG/Tema9/EjercicioInstrumentos/src/Piano.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+public class Piano extends Instrumento {
+    private int numTeclas;
+    private boolean deCola;
 
-public class Piano {
+    public Piano(String codigo, String marca, double precioBase, int anioFabricacion, int numTeclas, boolean deCola) {
+        super(codigo, marca, precioBase, anioFabricacion);
+        this.numTeclas = numTeclas;
+        this.deCola = deCola;
+    }
+
+    public double calcularPrecioFinal(){
+        double precioFinal = this.getPrecioBase();
+
+        if (this.deCola){
+            precioFinal += 600;
+        }
+
+        if (this.numTeclas > 61){
+            precioFinal += 100;
+        }
+
+        return precioFinal;
+    }
+
+    public boolean esEspecial(){
+        if (this.deCola || this.numTeclas > 61){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public String getTipo(){
+        return "Piano";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Piano{" +
+                "numTeclas=" + numTeclas +
+                ", deCola=" + deCola +
+                '}';
+    }
 }
--- PROG/Tema9/EjercicioInstrumentos/src/PruebaTiendaInstrumentos.java
+++ PROG/Tema9/EjercicioInstrumentos/src/PruebaTiendaInstrumentos.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
 public class PruebaTiendaInstrumentos {
+    public static void main(String[] args) {
+        Instrumento g1 = new Guitarra("G1", "Fender", 500, 2010, true, 6);
+        Instrumento g2 = new Guitarra("G2", "Gibson", 700, 1995, false, 7);
+        Instrumento g3 = new Guitarra("G3", "Yamaha", 300, 2005, false, 6);
+
+        Instrumento p1 = new Piano("P1", "Yamaha", 2000, 1990, 88, true);
+        Instrumento p2 = new Piano("P2", "Casio", 800, 2015, 61, false);
+        Instrumento p3 = new Piano("P3", "Kawai", 1500, 1980, 70, false );
+
+        TiendaInstrumentos tienda = new TiendaInstrumentos("Instrumentos Daniela");
+
+        tienda.añadirInstrumento(g1);
+        tienda.añadirInstrumento(g2);
+        tienda.añadirInstrumento(g3);
+        tienda.añadirInstrumento(p1);
+        tienda.añadirInstrumento(p2);
+        tienda.añadirInstrumento(p3);
+
+        Instrumento[] catalogo = tienda.getCatalogo();
+
+        for (int i = 0; i < 6; i++) {
+            System.out.println(catalogo[i].toString());
+        }
+
+        tienda.rebajarEspeciales(10);
+
+
+
+
+    }
 }
