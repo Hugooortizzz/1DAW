04ea13406ce46a1837d40943c78c41558f8a85ce
Hugooortizzz <hugo.ortiz-duran@iesruizgijon.com>
2026-04-16 15:36:36 +0200

    16/4/2026

---
+++ PROG/Tema10_2/src/Ejercicio9/Carta.java
@@ -0,0 +1,39 @@
+package Ejercicio9;
+
+import java.util.Random;
+
+public class Carta {
+    private Palo palo;
+    private int valor;
+    private static Palo[] palos = Palo.values(); //Array con los palos
+
+
+
+    public Carta() {
+        Random r = new Random();
+        this.valor = r.nextInt(1,13);
+        this.palo = palos[r.nextInt(0,4)];
+    }
+
+
+    public Palo getPalo() {
+        return palo;
+    }
+
+    public void setPalo(Palo palo) {
+        this.palo = palo;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return this.valor + " de " + this.palo;
+    }
+}
---
+++ PROG/Tema10_2/src/Ejercicio9/Palo.java
@@ -0,0 +1,5 @@
+package Ejercicio9;
+
+public enum Palo {
+    Bastos, Oros, Copas, Espadas;
+}
